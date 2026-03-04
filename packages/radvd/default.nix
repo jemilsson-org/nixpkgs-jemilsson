750a0d8c47bb3453dcd0809995bd3aa465d26ccf
Jonas Emilsson <jonas.emilsson@gmail.com>
2026-03-04 12:28:49 +0100

    radvd: f67335b5335b6ed5ca68d6fa71c08cccb4f3a629 -> 2.20

--- packages/radvd/default.nix
+++ packages/radvd/default.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   pname = "radvd";
-  version = "f67335b5335b6ed5ca68d6fa71c08cccb4f3a629";
+  version = "2.20";
 
   src = fetchFromGitHub {
     owner = "radvd-project";
     repo = "radvd";
-    rev = "${version}";
-    hash = "sha256-mkVQTtXq7RnAISy5cgO/mJ//ziqQdfUZk6LZtcoaLgk=";
+    rev = "v${version}";
+    hash = "sha256-090b8953cq7pvxf8i5wsippsi3zc8jxy559k6jpfjjmbbvl8zlmk";
   };
 
   nativeBuildInputs = [ pkg-config bison flex libbsd check autoreconfHook ];
